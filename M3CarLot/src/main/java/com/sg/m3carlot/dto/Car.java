d29566d258b743c0464702824dac0ff4d4e2e4f9
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-06-03 10:39:30 -0400

    Testing methods implemented for DTO

--- M3CarLot/src/main/java/com/sg/m3carlot/dto/Car.java
+++ M3CarLot/src/main/java/com/sg/m3carlot/dto/Car.java
@@ -1,6 +1,7 @@
 package com.sg.m3carlot.dto;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 /**
  *
@@ -15,7 +16,7 @@ public class Car {
     private BigDecimal price;
     private long odometerMiles;
     private CarKey key;
-    
+
     /*ctor*/
     public Car(String VIN, CarKey key) {
         this.VIN = VIN;
@@ -77,6 +78,63 @@ public class Car {
 
     public void setKey(CarKey key) {
         this.key = key;
+    }
+
+    /*testing*/
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + Objects.hashCode(this.VIN);
+        hash = 53 * hash + Objects.hashCode(this.make);
+        hash = 53 * hash + Objects.hashCode(this.model);
+        hash = 53 * hash + Objects.hashCode(this.color);
+        hash = 53 * hash + Objects.hashCode(this.price);
+        hash = 53 * hash + (int) (this.odometerMiles ^ (this.odometerMiles >>> 32));
+        hash = 53 * hash + Objects.hashCode(this.key);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Car other = (Car) obj;
+        if (this.odometerMiles != other.odometerMiles) {
+            return false;
+        }
+        if (!Objects.equals(this.VIN, other.VIN)) {
+            return false;
+        }
+        if (!Objects.equals(this.make, other.make)) {
+            return false;
+        }
+        if (!Objects.equals(this.model, other.model)) {
+            return false;
+        }
+        if (!Objects.equals(this.color, other.color)) {
+            return false;
+        }
+        if (!Objects.equals(this.price, other.price)) {
+            return false;
+        }
+        if (!Objects.equals(this.key, other.key)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "VIN=" + VIN + ", make=" + make + ", model=" + model
+                + ", color=" + color + ", price=" + price + ", odometerMiles="
+                + odometerMiles + ", key=" + key + '}';
     }
 
 }
--- M3CarLot/src/main/java/com/sg/m3carlot/dto/CarKey.java
+++ M3CarLot/src/main/java/com/sg/m3carlot/dto/CarKey.java
@@ -1,5 +1,7 @@
 package com.sg.m3carlot.dto;
 
+import java.util.Objects;
+
 /**
  *
  * @author naris
@@ -31,4 +33,38 @@ public class CarKey {
         this.laserCut = laserCut;
     }
 
+    /*testing*/
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.VIN);
+        hash = 29 * hash + (this.laserCut ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CarKey other = (CarKey) obj;
+        if (this.laserCut != other.laserCut) {
+            return false;
+        }
+        if (!Objects.equals(this.VIN, other.VIN)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CarKey{" + "VIN=" + VIN + ", laserCut=" + laserCut + '}';
+    }
 }
