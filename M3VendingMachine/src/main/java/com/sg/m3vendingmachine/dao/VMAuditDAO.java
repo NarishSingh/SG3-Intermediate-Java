72497d79335901ad6361af1f0e5b9c652295187a
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-06-08 15:23:53 -0400

    Audit DAO created and implemented

--- M3VendingMachine/src/main/java/com/sg/m3vendingmachine/dao/VMAuditDAO.java
+++ M3VendingMachine/src/main/java/com/sg/m3vendingmachine/dao/VMAuditDAO.java
@@ -1,6 +1,13 @@
 package com.sg.m3vendingmachine.dao;
 
 public interface VMAuditDAO {
-    
+
+    /**
+     * Create a time stamped entry of item addition or removal (via sale) from
+     * inventory
+     *
+     * @param entry {String} record of stocking or sale of an item
+     * @throws VendingPersistenceException if cannot write to audit file
+     */
     public void writeAuditEntry(String entry) throws VendingPersistenceException;
 }
---
+++ M3VendingMachine/src/main/java/com/sg/m3vendingmachine/dao/VMAuditDAOImpl.java
@@ -0,0 +1,26 @@
+package com.sg.m3vendingmachine.dao;
+
+import java.io.*;
+import java.time.LocalDateTime;
+
+public class VMAuditDAOImpl implements VMAuditDAO {
+
+    public static final String AUDIT_FILE = "audit.txt";
+
+    @Override
+    public void writeAuditEntry(String entry) throws VendingPersistenceException {
+        PrintWriter out;
+
+        try {
+            out = new PrintWriter(new FileWriter(AUDIT_FILE, true));
+        } catch (IOException e) {
+            throw new VendingPersistenceException("Could not persist stock/transaction record", e);
+        }
+
+        LocalDateTime timestamp = LocalDateTime.now();
+
+        out.println(timestamp.toString() + " : " + entry);
+        out.flush();
+    }
+
+}
