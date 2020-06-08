eb2ba147dda2aa1be5bf99af2099fb512a4401ed
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-06-08 14:28:17 -0400

    Change converted from DTO class to a helper class, moved to DAO

--- M3VendingMachine/src/main/java/com/sg/m3vendingmachine/dao/Change.java
+++ M3VendingMachine/src/main/java/com/sg/m3vendingmachine/dao/Change.java
@@ -1,9 +1,9 @@
 /*
-Change class
-should be constructed every transaction
+Change class helper class
  */
-package com.sg.m3vendingmachine.dto;
+package com.sg.m3vendingmachine.dao;
 
+import com.sg.m3vendingmachine.dto.*;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.HashMap;
@@ -12,20 +12,23 @@ import java.util.Map;
 public class Change {
 
     /*fields*/
-    private final BigDecimal cashIn;
-    private Map<Coins, Integer> changeBack;
+//    private final BigDecimal cashIn;
+//    private Map<Coins, Integer> changeBack;
     private static final BigDecimal PENNY_VALUE = new BigDecimal("0.01");
     private static final BigDecimal NICKEL_VALUE = new BigDecimal("0.05");
     private static final BigDecimal DIME_VALUE = new BigDecimal("0.10");
     private static final BigDecimal QUARTER_VALUE = new BigDecimal("0.25");
 
     /*ctor*/
+    /*
     public Change(BigDecimal cashIn) {
         changeBack = new HashMap<>();
         this.cashIn = cashIn;
     }
+    */
 
     /*getter/setters*/
+    /*
     public Map<Coins, Integer> getChangeBack() {
         return changeBack;
     }
@@ -33,20 +36,24 @@ public class Change {
     public void setChangeBack(Map<Coins, Integer> changeBack) {
         this.changeBack = changeBack;
     }
+    /*
 
     /*read-only*/
+    /*
     public BigDecimal getCashIn() {
         return cashIn;
     }
+    */
 
     /*behaviors*/
     /**
      * Figure out how much change to dispense, and set changeBack field
      * accordingly
      */
-    private void calculateChange(Item buying) {
+    private Map<Coins, Integer> calculateChange(BigDecimal cashIn, Item buying) {
         BigDecimal price = buying.getCost();
-        BigDecimal change = this.getCashIn().subtract(price);
+//        BigDecimal change = this.getCashIn().subtract(price);
+        BigDecimal change = cashIn.subtract(price);
         Map<Coins, Integer> changeCoins = new HashMap<>();
 
         int quarterCt = change.divide(QUARTER_VALUE, 0, RoundingMode.HALF_UP).intValue();
@@ -64,6 +71,8 @@ public class Change {
         int pennyCt = changeAfterNickels.divide(PENNY_VALUE, 0, RoundingMode.HALF_UP).intValue();
         changeCoins.put(Coins.PENNIES, pennyCt);
 
-        this.setChangeBack(changeCoins);
+//        this.setChangeBack(changeCoins);
+        
+        return changeCoins;
     }
 }
