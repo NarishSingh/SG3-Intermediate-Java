587e75717fe7783155a5f92c95af36a03c19e3d7
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-06-09 14:46:45 -0400

    fixed calculateChange() so that it throws an exception is insufficient funds

--- M3VendingMachine/src/main/java/com/sg/m3vendingmachine/dao/Change.java
+++ M3VendingMachine/src/main/java/com/sg/m3vendingmachine/dao/Change.java
@@ -4,6 +4,7 @@ Change class helper class
 package com.sg.m3vendingmachine.dao;
 
 import com.sg.m3vendingmachine.dto.*;
+import com.sg.m3vendingmachine.service.InsufficientFundsException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.HashMap;
@@ -12,38 +13,10 @@ import java.util.Map;
 public class Change {
 
     /*fields*/
-//    private final BigDecimal cashIn;
-//    private Map<Coins, Integer> changeBack;
     private static final BigDecimal PENNY_VALUE = new BigDecimal("0.01");
     private static final BigDecimal NICKEL_VALUE = new BigDecimal("0.05");
     private static final BigDecimal DIME_VALUE = new BigDecimal("0.10");
     private static final BigDecimal QUARTER_VALUE = new BigDecimal("0.25");
-
-    /*ctor*/
- /*
-    public Change(BigDecimal cashIn) {
-        changeBack = new HashMap<>();
-        this.cashIn = cashIn;
-    }
-     */
-
- /*getter/setters*/
- /*
-    public Map<Coins, Integer> getChangeBack() {
-        return changeBack;
-    }
-
-    public void setChangeBack(Map<Coins, Integer> changeBack) {
-        this.changeBack = changeBack;
-    }
-    /*
-
-    /*read-only*/
- /*
-    public BigDecimal getCashIn() {
-        return cashIn;
-    }
-     */
 
     /*behaviors*/
     /**
@@ -52,29 +25,33 @@ public class Change {
      * @param cashIn {BigDecimal} cash inputted by user
      * @param buying {Item} the consumable the user intends to buy
      * @return {Map} a HashMap of change in coins to be dispensed to the user
+     * @throws InsufficientFundsException if user didn't input enough money
      */
-    static public Map<Coins, Integer> calculateChange(Item buying, BigDecimal cashIn) {
-        BigDecimal price = buying.getCost();
-//        BigDecimal change = this.getCashIn().subtract(price);
-        BigDecimal change = cashIn.subtract(price);
+    static public Map<Coins, Integer> calculateChange(Item buying, BigDecimal cashIn)
+            throws InsufficientFundsException {
         Map<Coins, Integer> changeCoins = new HashMap<>();
+        BigDecimal price = buying.getCost();
+        BigDecimal change = cashIn.subtract(price);
 
-        int quarterCt = change.divide(QUARTER_VALUE, 0, RoundingMode.HALF_UP).intValue();
-        changeCoins.put(Coins.QUARTERS, quarterCt);
-        BigDecimal changeAfterQuarters = change.subtract(QUARTER_VALUE.multiply(new BigDecimal(String.valueOf(quarterCt))));
+        if (change.compareTo(BigDecimal.ZERO) < 0) {
+            throw new InsufficientFundsException("Not enough money to buy item");
+        } else {
+            int quarterCt = change.divide(QUARTER_VALUE, 0, RoundingMode.HALF_UP).intValue();
+            changeCoins.put(Coins.QUARTERS, quarterCt);
+            BigDecimal changeAfterQuarters = change.subtract(QUARTER_VALUE.multiply(new BigDecimal(String.valueOf(quarterCt))));
 
-        int dimeCt = changeAfterQuarters.divide(DIME_VALUE, 0, RoundingMode.HALF_UP).intValue();
-        changeCoins.put(Coins.DIMES, dimeCt);
-        BigDecimal changeAfterDimes = changeAfterQuarters.subtract(DIME_VALUE.multiply(new BigDecimal(String.valueOf(dimeCt))));
+            int dimeCt = changeAfterQuarters.divide(DIME_VALUE, 0, RoundingMode.HALF_UP).intValue();
+            changeCoins.put(Coins.DIMES, dimeCt);
+            BigDecimal changeAfterDimes = changeAfterQuarters.subtract(DIME_VALUE.multiply(new BigDecimal(String.valueOf(dimeCt))));
 
-        int nickelCt = changeAfterDimes.divide(NICKEL_VALUE, 0, RoundingMode.HALF_UP).intValue();
-        changeCoins.put(Coins.NICKELS, nickelCt);
-        BigDecimal changeAfterNickels = changeAfterDimes.subtract(NICKEL_VALUE.multiply(new BigDecimal(String.valueOf(nickelCt))));
+            int nickelCt = changeAfterDimes.divide(NICKEL_VALUE, 0, RoundingMode.HALF_UP).intValue();
+            changeCoins.put(Coins.NICKELS, nickelCt);
+            BigDecimal changeAfterNickels = changeAfterDimes.subtract(NICKEL_VALUE.multiply(new BigDecimal(String.valueOf(nickelCt))));
 
-        int pennyCt = changeAfterNickels.divide(PENNY_VALUE, 0, RoundingMode.HALF_UP).intValue();
-        changeCoins.put(Coins.PENNIES, pennyCt);
+            int pennyCt = changeAfterNickels.divide(PENNY_VALUE, 0, RoundingMode.HALF_UP).intValue();
+            changeCoins.put(Coins.PENNIES, pennyCt);
 
-//        this.setChangeBack(changeCoins);
-        return changeCoins;
+            return changeCoins;
+        }
     }
 }
