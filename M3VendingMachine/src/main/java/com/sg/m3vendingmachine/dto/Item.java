6be0bb1304ab7bbcc330ea4ac04b680ac725ff29
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-06-10 11:43:49 -0400

    testing methods added

--- M3VendingMachine/src/main/java/com/sg/m3vendingmachine/dto/Item.java
+++ M3VendingMachine/src/main/java/com/sg/m3vendingmachine/dto/Item.java
@@ -4,6 +4,7 @@ Item DTO class, populates Vending Machine DTO
 package com.sg.m3vendingmachine.dto;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Item {
 
@@ -33,6 +34,41 @@ public class Item {
 
     public void setCost(BigDecimal cost) {
         this.cost = cost;
+    }
+
+    /*testing methods*/
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.name);
+        hash = 29 * hash + Objects.hashCode(this.cost);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Item other = (Item) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.cost, other.cost)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "name=" + name + ", cost=" + cost + '}';
     }
 
 }
